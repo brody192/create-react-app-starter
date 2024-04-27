916cd78ac92a454fcdd5e9df7c91b5e924b7de39
Filip <42967255+filipzakrocki@users.noreply.github.com>
2024-04-27 19:52:21 +0100

    Adding typescript support

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App(): JSX.Element {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Choo Choo! This is an example of a create-react-app site running on Railway. Fiflak</p>
+        <p>Get ready to hack!</p>
         <a className="App-link" href="https://react.dev/learn" target="_blank" rel="noreferrer noopener">Learn React</a>
       </header>
     </div>
