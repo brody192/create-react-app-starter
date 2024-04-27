89acb347c5752ca79a329b581ee5e06a83e1da46
Filip <42967255+filipzakrocki@users.noreply.github.com>
2024-04-27 19:10:33 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Choo Choo! This is an example of a create-react-app site running on Railway.</p>
+        <p>Choo Choo! This is an example of a create-react-app site running on Railway. Fiflak</p>
         <a className="App-link" href="https://react.dev/learn" target="_blank" rel="noreferrer noopener">Learn React</a>
       </header>
     </div>
